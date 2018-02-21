d4cb246bd0befc5609238b53158c8f86b3805b4a
Matt McCarthy <34220681+mccartmz@users.noreply.github.com>
2018-02-21 16:44:05 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-//create variables
 var wins = 0;
 var showWin = document.getElementById("winNum");
 var losses = 0;
@@ -16,56 +15,41 @@ function random() {
 }
 random();
 
-//gather user input from keypress
+//create else if statements
 document.onkeyup = function(e) {
-    //PRESS ENTER TO RESET GAME
     if (e.which === 13) {
-        //reset win/loss count/display
         wins = 0;
         losses = 0;
         showWin.innerHTML = wins;
         showLoss.innerHTML = losses;
-        //reset guesses
         remaining = 9;
         showLeft.innerHTML = remaining;
         guesses = [];
         showGuess.innerHTML = "";
-        //call new psychicLetter
         random();
-    //check that there are guesses remaining and entry is a letter
     } else if (remaining > 0 && e.which >= 65 && e.which <= 90) {
-        //assign key to userGuess
         var userGuess = e.key;
-        //check for win condition
         if (userGuess.toUpperCase() === psychicLetter) {
-            //add to win count
             wins++;
-            alert("You must be psychic! The letter was " + psychicLetter + ".");
+            alert("You guessed right! The letter was " + psychicLetter + ".");
             showWin.innerHTML = wins;
-            //reset guesses
             remaining = 9;
             guesses = [];
             showGuess.innerHTML = "";
-            //call new psychicLetter
             random();
         } else {
-            //add each guess to the guesses array
             guesses.push(" " + userGuess);
             showGuess.innerHTML = guesses;
-            //decrement remaining guesses
             remaining--;
             showLeft.innerHTML = remaining;
         }
     } else if (remaining === 0) {
-        //add to losses count
         losses++;
-        alert("Out of guesses; you lose this round.");
+        alert("No more guesses. Press Enter to try again.");
         showLoss.innerHTML = losses;
-        //reset guesses
         remaining = 9;
         guesses = [];
         showGuess.innerHTML = "";
-        //call new psychicLetter
         random();
     }
 };
